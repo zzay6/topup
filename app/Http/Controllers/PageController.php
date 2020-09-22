c4b503fc04ded0959e3686c184e367eb9258d956
damari227 <gaming29zacky@gmail.com>
2020-09-22 22:51:48 +0700

    Membuat sistem pencarian produk

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -12,4 +12,18 @@ class PageController extends Controller
         $produk = Produk::all();
         return view('index', compact('produk'));
     }
+
+
+    public function search(Request $req)
+    {
+    	if(empty($req->q)){
+    		return view('search', ['result' => Produk::all()]);
+    	}
+    	$result = Produk::where('nama','LIKE','%'.$req->q.'%')->get();
+    	if($result->isEmpty()){
+    		return view('search', ['noresult' => 'Produk tidak di temukan']);
+    	} else {
+    		return view('search', compact('result'));
+    	}
+    }
 }
