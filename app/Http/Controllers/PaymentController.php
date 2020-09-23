d8c6b1d87ca24669b93b7d8960a84f2cac7673ca
damari227 <gaming29zacky@gmail.com>
2020-09-23 21:03:21 +0700

    Membuat handle permintaan pembayaran

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Transactions;
+use App\Models\Produk;
+use App\Models\Items;
 
 class PaymentController extends Controller
 {
 	// Handle payment request from user
     public function paymentRequest(Request $req){
-    	// 
+    	$item = Items::where('id',$req->item)->first();
+    	$product = Produk::where('pulsa_op',$item->pulsa_op)->first();
+
+    	$transaction = Transactions::max('id');
+
+    	$transactionId = 'TY'.sprintf('%08s', $transaction + 1);
+    	$transaction = ['transaction id' => $transactionId];
+    	return response($transaction);
     }
 }
