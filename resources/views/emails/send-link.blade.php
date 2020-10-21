cfd323fb11b35cf301df862f1d310227a838aadc
damari227 <gaming29zacky@gmail.com>
2020-10-21 16:01:17 +0700

    Menambahkan sedikit teks pada view email

--- resources/views/emails/send-link.blade.php
+++ resources/views/emails/send-link.blade.php
@@ -1,18 +1,16 @@
 @component('mail::message')
-<div style="width: 100%">
-<h2>Permintaan untuk mengganti kata sandi</h2>
+# Permintaan perubahan kata sandi
 
 Halo {{ $user->name }},<br>
-link untuk mengganti kata sandi akun mu telah siap
+Permintaan untuk mengganti kata sandi telah diterima <br>
+Klik link dibawah ini untuk mengganti kata sandi
 <p>
 	Jangan berikan link ini kepada siapapun(link ini bersifat rahasia)
 </p>
-<a href="{{ $url }}">{{ $url }}</a>
 @component('mail::button', ['url' => $url])
-Klik disini untuk mengganti kata sandi
+Klik disini
 @endcomponent
 
 Terima kasih,<br>
 {{ config('app.name') }}
-</div>
 @endcomponent
